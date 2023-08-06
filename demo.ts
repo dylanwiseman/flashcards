391461693cf2d25024bf36767f1c3da13aa30aab
Dylan Wiseman <dylan.wiseman22@gmail.com>
2023-08-05 20:37:41 -0600

    dictionary

---
+++ demo.ts
@@ -0,0 +1,15 @@
+import { readingBeginning, setup as setupJmdict } from "jmdict-simplified-node";
+
+if (module === require.main) {
+  (async function main() {
+    const jmdictPromise = setupJmdict(
+      "my-jmdict-simplified-db",
+      "jmdict-eng-3.1.0.json"
+    );
+    const { db } = await jmdictPromise;
+    // This `db` is used by all functions in this API, so hang on to it.
+    // You only need to run `setup`/`setupJmdict` ONCE in your app, to get this `db`.
+    const results = await readingBeginning(db, "あおい", 3);
+    console.dir(results, { depth: null });
+  })();
+}
